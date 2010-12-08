564691194f6118660ccb71a789ece2d8c6681d16
Vivien Barousse <barousse.vivien@gmail.com>
2010-12-08 03:20:41 +0000

    Removd useless imports

--- src/main/java/com/aperigeek/gtsp/ui/GtspFrame.java
+++ src/main/java/com/aperigeek/gtsp/ui/GtspFrame.java
@@ -19,8 +19,6 @@ import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
